af79f90e4330cad5cd29c59a08df859e0b5768e2
PacoReinaCampo <frareicam@gmail.com>
2023-04-20 00:25:12 +0200

    Working in OpenNN 🔑; 🔨; 🚀; 🦀

--- doc/devops/assets/chapter6/draw.sh
+++ doc/devops/assets/chapter6/draw.sh
@@ -43,9 +43,3 @@
 ###################################################################################
 
 dot -Tsvg dependences-global.dot > dependences-global.svg
-
-dot -Tsvg hardware-project.dot > hardware-project.svg
-dot -Tsvg software-project.dot > software-project.svg
-
-dot -Tpng hardware-project.dot > hardware-project.png
-dot -Tpng software-project.dot > software-project.png
